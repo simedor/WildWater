5bf601211d6ce57171cf96806b327ff2e519b856
timothee23-gh <timothee.renouf23@gmail.com>
2025-12-02 11:40:14 +0100

    Update header.h
    proto + fin struct usine

--- codeC/header.h
+++ codeC/header.h
@@ -1,9 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 typedef struct usine {
   char* ID;
   float capte;
+  float capacitemax;
+  float conso;
 }Usine;
 typedef struct avl {
   struct *Usine u;
   struct *avl fg;
   struct *avl fd;
-}AVL;
+  int eq;
+}AVL, *pAVL;
+
+Arbre* creerArbre(char* id);
+
+Arbre* insertionAVL(Arbre* a, char* id, long volume, int* h_change);
+
+Arbre* equilibreAVL(Arbre* a);
+
+void libererArbre(Arbre* a);
+
+void afficherArbreInverse(Arbre* a);
+
+int max(int a, int b);
+int min(int a, int b);
