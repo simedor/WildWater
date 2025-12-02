8dee0665444c71e5652bb2b6eb27ea4b4601aed5
simedor <108340948+simedor@users.noreply.github.com>
2025-12-02 11:37:52 +0100

    Update output.c
    fonctions parcoursInfixeInverse

--- codeC/output.c
+++ codeC/output.c
@@ -1,1 +1,12 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include "header.h"
 
+void parcours_infixe_inverse(Arbre* a, FILE* f, char* mode) {
+  if (a != NULL) {
+    parcours_infixe_inverse(a->droit, f, mode);
+    afficher_noeud(a->usine, f, mode);
+    parcours_infixe_inverse(a->gauche, f, mode);
+  }
+}
