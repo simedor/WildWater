ecf12dd9e99d4536565390f3f71e842f13367efa
simedor <108340948+simedor@users.noreply.github.com>
2025-12-04 09:04:58 +0100

    Update output.c
    creer fichier csv de sortie

--- codeC/output.c
+++ codeC/output.c
@@ -3,10 +3,24 @@
 # include <string.h>
 # include "header.h"
 
-void parcours_infixe_inverse(Arbre* a, FILE* f, char* mode) {
+void afficher_noeud(Usine* u, FILE* f) {
+    fprintf(f, "%s;%ld\n", u->id, u->capacite);
+}
+
+void parcours_infixe_inverse(Arbre* a, FILE* f) {
   if (a != NULL) {
-    parcours_infixe_inverse(a->droit, f, mode);
-    afficher_noeud(a->usine, f, mode);
-    parcours_infixe_inverse(a->gauche, f, mode);
+    parcours_infixe_inverse(a->droit, f);
+    afficher_noeud(a->usine, f);
+    parcours_infixe_inverse(a->gauche, f);
   }
 }
+
+void output_histo(char* nom_fichier, Arbre* racine, char* mode) {
+    FILE* f = fopen(nom_fichier, "w");
+    if (f == NULL) {
+        fprintf(stderr, "Erreur : Impossible de créer le fichier %s\n", nom_fichier);
+        exit(2);
+    }
+    fprintf(f, "identifier;max volume (k.m3.year-1)\n");
+    parcours_infixe_inverse(racine, f, mode);
+    fclose(f);
