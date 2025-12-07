e70a6f49bb4d1841c864e0d9304ba36bb1d6afa0
simedor <108340948+simedor@users.noreply.github.com>
2025-12-07 22:22:32 +0100

    Update header.h
    Changement des noms des variables contenues dans la structure Usine (pour plus de compréhension), ajout des prototypes manquants (fonctions de rotation, fonctions dans input.c et output.c), ajout de commentaires

--- codeC/header.h
+++ codeC/header.h
@@ -4,29 +4,52 @@
 #include <time.h>
 #include <string.h>
 #include <math.h>
-typedef struct usine {
-  char* ID;
-  float capte;
-  float capacitemax;
-  float conso;
-}Usine;
 
+/*
+Structure représentant les données d'une usine
+- volumeSource : Somme des volumes captés (utilisé pour histo src)
+- capacite : Capacité maximale (utilisé pour histo max)
+- volumeTraite : Volume réel (utilisé pour histo real)
+- ID : Identifiant unique
+*/
+typedef struct usine {
+  char* ID[50];
+  double volumeSource;
+  double capacite;
+  double volumeTraite;
+} Usine;
+
+/*
+Structure de l'arbre AVL, contenant un pointeur vers une usine
+*/
 typedef struct AVL {
-  struct *Usine u;
+  *Usine u;
   struct *AVL fg;
   struct *AVL fd;
   int equilibre;
-}AVL, *pAVL;
+} AVL, *pAVL;
 
-pAVL creerArbre(char* id, float ct, float capamax, float cs);
-
-pAVL insertionAVL(pAVL a, char* id, float volume, int* h);
-
-pAVL equilibreAVL(pAVL a);
-
-void libererArbre(pAVL a);
-
-void afficherArbreInverse(pAVL a);
+// Module avl.c
 
 int max(int a, int b);
 int min(int a, int b);
+pAVL rotationGauche(pAVL a);
+pAVL rotationDroite(pAVL a);
+pAVL rotationGaucheDouble(pAVL a);
+pAVL rotationDroiteDouble(pAVL a);
+pAVL creerArbre(Usine u);
+pAVL insertionAVL(pAVL a, Usine u, int* h);
+pAVL equilibreAVL(pAVL a);
+void libererArbre(pAVL a);
+
+// Module input.c
+
+void nettoyerLigne(char* chaine);
+int estNumerique(char* chaine);
+void chargerDonnees(char* cheminFichier, Arbre** a);
+
+// Module output.c
+
+void parcoursInfixeInverse(Arbre* a, FILE* f);
+void afficherNoeud(Usine* u, FILE* f);
+void output_histo(char* nom_fichier, Arbre* a);
