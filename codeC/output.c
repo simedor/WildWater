ba0fc1a99ad0fd20646bc76108187da2635e9617
A.G. <alexandre.godineau@etu.cyu.fr>
2025-12-08 11:25:55 +0100

    Changement nom variable (fichier)

--- codeC/output.c
+++ codeC/output.c
@@ -3,19 +3,19 @@
 /*
 Ecrire une ligne de données pour une usine et gère la conversion d'unités
 */
-void afficherNoeud(Usine* u, FILE* f) {
+void afficherNoeud(Usine* u, FILE* fichier) {
     double volume = u->volumeSource / 1000.0;
-    fprintf(f, "%s;%.3f\n", u->ID, volume);
+    fprintf(fichier, "%s;%.3f\n", u->ID, volume);
 }
 
 /*
 Ecrire les identifiants dans l'ordre alphabétique inverse
 */
-void parcoursInfixeInverse(Arbre* a, FILE* f) {
+void parcoursInfixeInverse(Arbre* a, FILE* fichier) {
   if (a != NULL) {
-    parcoursInfixeInverse(a->fd, f);
-    afficherNoeud(a->u, f);
-    parcoursInfixeInverse(a->fg, f);
+    parcoursInfixeInverse(a->fd, fichier);
+    afficherNoeud(a->u, fichier);
+    parcoursInfixeInverse(a->fg, fichier);
   }
 }
 
@@ -23,12 +23,12 @@ void parcoursInfixeInverse(Arbre* a, FILE* f) {
 Générer le fichier CSV pour faire l'histogramme
 */
 void outputHistoSource(char* nomFichier, Arbre* a) {
-    FILE* f = fopen(nomFichier, "w");
-    if (f == NULL) {
+    FILE* fichier = fopen(nomFichier, "w");
+    if (fichier == NULL) {
         fprintf(stderr, "Erreur : Impossible de créer le fichier %s\n", nomFichier);
         exit(1);
     }
     fprintf(f, "identifier;source volume (k.m3.year-1)\n");
-    parcoursInfixeInverse(a, f);
-    fclose(f);
+    parcoursInfixeInverse(a, fichier);
+    fclose(fichier);
 }
