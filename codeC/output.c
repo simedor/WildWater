e5727c9e00dc16bb372b9c214fa689bed20dadd6
simedor <108340948+simedor@users.noreply.github.com>
2025-12-13 05:21:21 +0100

    Update output.c
    Ajout de commentaires, ajout de vérification pour la gestion d'erreurs (si le paramètre mode est inconnu)

--- codeC/output.c
+++ codeC/output.c
@@ -1,9 +1,10 @@
 #include "header.h"
 
 /*
-Ecrire une ligne de données pour une usine et gère la conversion d'unités
+Écrit les données d'une usine dans le fichier selon le mode choisis (src, max, real) et gère la conversion d'unité.
 */
 void afficherNoeud(Usine* u, FILE* fichier, char* mode) {
+    if (u == NULL || fichier == NULL) exit(1);
     double valeur = 0;
     if (strcmp(mode, "src") == 0) {
         valeur = u->volumeSource;
@@ -11,12 +12,16 @@ void afficherNoeud(Usine* u, FILE* fichier, char* mode) {
         valeur = u->capacite;
     } else if (strcmp(mode, "real") == 0) {
         valeur = u->volumeTraite;
+    } else {
+        fprintf(stderr, "Erreur : Mode '%s' non reconnu.\n", mode);
+        fclose(fichier);
+        exit(1);
     }
     fprintf(fichier, "%s;%.3f\n", u->ID, valeur / 1000.0);
 }
 
 /*
-Ecrire les identifiants dans l'ordre alphabétique inverse
+Ecrit les données dans l'ordre alphabétique inverse des identifiants.
 */
 void parcoursInfixeInverse(pAVL a, FILE* fichier, char* mode) {
   if (a != NULL) {
@@ -35,14 +40,23 @@ void outputHisto(char* nomFichier, pAVL a, char* mode) {
         fprintf(stderr, "Erreur : Impossible de créer le fichier %s\n", nomFichier);
         exit(1);
     }
+
+    // Ecriture de l'en-tête CSV
     if (strcmp(mode, "src") == 0) {
         fprintf(fichier, "identifier;volume source(M.m3.year-1)\n");
     } else if (strcmp(mode, "max") == 0) {
         fprintf(fichier, "identifier;capacite(M.m3.year-1)\n");
     } else if (strcmp(mode, "real") == 0) {
         fprintf(fichier, "identifier;volume traite(M.m3.year-1)\n");
+    } else {
+        fprintf(stderr, "Erreur : Mode '%s' non reconnu.\n", mode);
+        fclose(fichier);
+        exit(1);
     }
+
+    // Ecriture des données
     parcoursInfixeInverse(a, fichier, mode);
+    
     fclose(fichier);
 }
 
