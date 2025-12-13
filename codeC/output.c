689f8266ff366274abe57d82ea14886161d810bf
simedor <108340948+simedor@users.noreply.github.com>
2025-12-13 06:13:02 +0100

    Update output.c
    Ajout fonction afficherNoeudLeaks, parcoursInfixe, outputLeaks + commentaires

--- codeC/output.c
+++ codeC/output.c
@@ -1,7 +1,8 @@
 #include "header.h"
 
 /*
-Écrit les données d'une usine dans le fichier selon le mode choisis (src, max, real) et gère la conversion d'unité.
+Écrit les données d'une usine dans le fichier selon le mode choisis (src, max, real).
+Utilisée par outputHisto.
 */
 void afficherNoeud(Usine* u, FILE* fichier, char* mode) {
     if (u == NULL || fichier == NULL) exit(1);
@@ -21,6 +22,18 @@ void afficherNoeud(Usine* u, FILE* fichier, char* mode) {
 }
 
 /*
+Écrit les données d'une usine dans le fichier pour le rapport de fuites.
+Utilisée par outputLeaks.
+*/
+void afficherNoeudLeaks(Usine* u, FILE* fichier) {
+    if (u == NULL || fichier == NULL) exit(1);
+    if (u->volumeTraite > u->capacite) {
+        double diff = u->volumeTraite - u->capacite;
+        fprintf(fichier, "%s;%.3f;%.3f;%.3f\n", u->ID, u->capacite / 1000.0, u->volumeTraite / 1000.0, diff / 1000.0);
+    }
+}
+
+/*
 Ecrit les données dans l'ordre alphabétique inverse des identifiants.
 */
 void parcoursInfixeInverse(pAVL a, FILE* fichier, char* mode) {
@@ -29,6 +42,17 @@ void parcoursInfixeInverse(pAVL a, FILE* fichier, char* mode) {
     afficherNoeud(a->u, fichier, mode);
     parcoursInfixeInverse(a->fg, fichier, mode);
   }
+}
+
+/*
+Ecrit les données dans l'ordre alphabétique des identifiants.
+*/
+void parcoursInfixe(pAVL a, FILE* fichier) {
+    if (a != NULL) {
+        parcoursInfixe(a->fg, fichier);
+        afficherNoeudLeaks(a->u, fichier);
+        parcoursInfixe(a->fd, fichier);
+    }
 }
 
 /* 
@@ -60,3 +84,18 @@ void outputHisto(char* nomFichier, pAVL a, char* mode) {
     fclose(fichier);
 }
 
+void outputLeaks(char* nomFichier, pAVL a) {
+    FILE* fichier = fopen(nomFichier, "w");
+    if (fichier == NULL) {
+        fprintf(stderr, "Erreur : Impossible de créer le fichier %s\n", nomFichier);
+        exit(1);
+    }
+
+    // Ecriture de l'en-tête CSV
+    fprintf(fichier, "identifier;capacite;volume traite;exces(M.m3.year-1)\n");
+
+    // Ecriture des données
+    parcoursInfixe(a, fichier);
+    
+    fclose(fichier);
+}
